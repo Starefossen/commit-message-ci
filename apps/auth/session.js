5480b1e7457300ab08f0f2893f5f3642c8e81ce8
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-01-22 23:03:05 +0100

    fix(cookie): disable secure cookies while waiting for SSL cert

--- apps/auth/session.js
+++ apps/auth/session.js
@@ -2,11 +2,12 @@
 
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+const redis = require('../../lib/redis');
 
 module.exports = session({
   resave: false,
   saveUninitialized: false,
   secret: process.env.APP_SECRET,
-  secure: process.env.NODE_ENV === 'production',
-  store: new RedisStore({host: 'redis'}),
+  // secure: process.env.NODE_ENV === 'production',
+  store: new RedisStore({client: redis}),
 });
